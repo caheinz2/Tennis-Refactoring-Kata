eb91c0d66f735b4e15746450dda87ccf37b44282
Caleb Heinz <caleb.heinz@7factor.io>
2026-01-30 12:20:46 -0500

    Created helper methods for score conditions and calculations based on game state.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -20,58 +20,87 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (this.isTied())
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-
-            }
+            return getScoreInTie();
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (isOvertime())
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            return getScoreInOvertime();
         }
         else
         {
-            for (int i=1; i<3; i++)
+            return getScoreInRegulation();
+        }
+    }
+
+    private boolean isTied() {
+        return m_score1==m_score2;
+    }
+
+    private boolean isOvertime() {
+        return m_score1>=4 || m_score2>=4;
+    }
+
+    private String getScoreInTie() {
+        String score = "";
+        switch (m_score1)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+
+        return score;
+    }
+
+    private String getScoreInOvertime() {
+        String score = "";
+
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+
+        return score;
+    }
+
+    private String getScoreInRegulation() {
+        String score = "";
+        int tempScore;
+
+        for (int i=1; i<3; i++)
+        {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            switch(tempScore)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+                case 0:
+                    score+="Love";
+                    break;
+                case 1:
+                    score+="Fifteen";
+                    break;
+                case 2:
+                    score+="Thirty";
+                    break;
+                case 3:
+                    score+="Forty";
+                    break;
             }
         }
+
         return score;
     }
 }
